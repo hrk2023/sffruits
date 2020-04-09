5b7ad59e10bd2c8151ca684941d4b39b7383cd73
rittick-dasgupta1234 <hrkdasgupta025@gmail.com>
2020-04-09 21:48:10 +0530

    base code

---
+++ static/js/animation.js
@@ -0,0 +1,9 @@
+const slider = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click',() => {
+        nav.classList.toggle('nav-active');
+    })
+}
+slider();
